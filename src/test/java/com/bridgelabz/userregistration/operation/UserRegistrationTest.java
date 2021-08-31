d24920fe88fc5ea1b9ca1aecc6543945e4dab5e4
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-31 21:40:31 +0530

    [kshitija]:first name is validated

--- src/test/java/com/bridgelabz/userregistration/operation/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistration/operation/UserRegistrationTest.java
@@ -1,5 +1,18 @@
 package com.bridgelabz.userregistration.operation;
 
+import org.junit.Test;
+
+import com.bridgelabz.userregistration.UserRegistration;
+
+import org.junit.Assert;
+
 public class UserRegistrationTest {
 
+	
+	@Test
+	public void giveFirstName_whenProper_ShouldReturnTrue() {
+		UserRegistration validater=new UserRegistration();
+		boolean result=validater.validateFirstName("Kshitija");
+		Assert.assertEquals(true, result);
+	}
 }
